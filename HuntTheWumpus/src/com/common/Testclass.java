f3966975c5c182d865f13ca9ba7981941e841e8b
Viviana Mardones <viviana.i.mardones@gmail.com>
2016-03-04 16:48:50 +0000

    hello!

---
+++ HuntTheWumpus/src/com/common/Testclass.java
@@ -0,0 +1,13 @@
+package com.common;
+
+public class Testclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int first = 2;
+		int second = 3;
+		int sum = first + second;
+		System.out.println("Hello, world");
+	}
+
+}
