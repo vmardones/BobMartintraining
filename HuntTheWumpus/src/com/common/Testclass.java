067ed8ec61ccee995a997e48547b045c58069488
Viviana Mardones <viviana.i.mardones@gmail.com>
2016-03-04 17:17:02 +0000

    hi

--- HuntTheWumpus/src/com/common/Testclass.java
+++ HuntTheWumpus/src/com/common/Testclass.java
@@ -4,10 +4,24 @@ public class Testclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int first = 2;
+		/*int first = 2;
 		int second = 3;
-		int sum = first + second;
-		System.out.println(sum);
+		int sum = first + second;*/
+		
+		int output = testloop(1);
+		
+		System.out.println(output);
+	}
+	
+	public static int testloop(int input) {
+		
+		int counter = input;
+		
+		while (counter < 9) {
+			counter++;
+		}
+		
+		return counter;
 	}
 
 }
